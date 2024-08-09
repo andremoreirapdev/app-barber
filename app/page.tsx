42a18fe4ba3be9f185923427c5837d83e6fff242
andremoreirapdev <andre.moreirap.dev@gmail.com>
2024-08-09 00:22:20 -0300

    chore: add prisma, seed, tailwind and shadcn-ui setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./_components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-red-500 mb-5 bg-red-50">Home</h1>
+      <Button>Teste</Button>
+    </>
+  );
+}
