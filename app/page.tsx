25777ead5df87ce134568195100fe7551ded1a38
andremoreirapdev <andre.moreirap.dev@gmail.com>
2024-08-09 00:38:26 -0300

    chore: update shadcn theme

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "./_components/ui/button";
 export default function Home() {
   return (
     <>
-      <h1 className="text-red-500 mb-5 bg-red-50">Home</h1>
+      <h1 className="text-red-500 mb-5">Home</h1>
       <Button>Teste</Button>
     </>
   );
