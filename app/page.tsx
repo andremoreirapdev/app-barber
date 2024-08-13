e03a1e21d5449fabbbbe8f5abb49a11903b70f88
andremoreirapdev <andre.moreirap.dev@gmail.com>
2024-08-13 18:50:49 -0300

    feat: add componente Header e Banner na Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
+import { SearchIcon } from "lucide-react";
+import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
+import { Input } from "./_components/ui/input";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      <h1 className="text-red-500 mb-5">Home</h1>
-      <Button>Teste</Button>
-    </>
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, André!</h2>
+        <p>Sexta-feira, 09 de agosto.</p>
+
+        <div className="flex flex-row items-center gap-2 mt-6">
+          <Input placeholder="Pesquisar"/>
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image alt="Banner 01" src="/banner-01.png" fill className="rounded-xl object-cover" />
+        </div>
+      </div>
+    </div>
   );
 }
