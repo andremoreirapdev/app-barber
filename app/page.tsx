9f54c12f3c3ffa309c5be201c31db5cd3720281b
andremoreirapdev <andre.moreirap.dev@gmail.com>
2024-08-14 16:06:57 -0300

    feat: add agendamento item na Home

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,10 @@ import Header from './_components/header'
 import { Button } from './_components/ui/button'
 import { Input } from './_components/ui/input'
 import Image from 'next/image'
+import { Card, CardContent } from './_components/ui/card'
+import { Badge } from './_components/ui/badge'
+import { Avatar } from './_components/ui/avatar'
+import { AvatarImage } from '@radix-ui/react-avatar'
 
 export default function Home() {
   return (
@@ -19,6 +23,7 @@ export default function Home() {
           </Button>
         </div>
 
+        {/* IMAGEM */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             alt="Banner 01"
@@ -27,6 +32,31 @@ export default function Home() {
             className="rounded-xl object-cover"
           />
         </div>
+
+        {/* AGENDAMENTO */}
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* ESQUERDA */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia The Barber&#39s Shop</p>
+              </div>
+            </div>
+
+            {/* DIREITA */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">14</p>
+              <p className="text-sm">18:30</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
