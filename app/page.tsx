142f746e3c8ed7b8781525642b2c21d0977c2084
andremoreirapdev <andre.moreirap.dev@gmail.com>
2024-08-14 12:30:23 -0300

    chore: finalização da conf. de setup com husky e lint-staged

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,33 @@
-import { SearchIcon } from "lucide-react";
-import Header from "./_components/header";
-import { Button } from "./_components/ui/button";
-import { Input } from "./_components/ui/input";
-import Image from "next/image";
+import { SearchIcon } from 'lucide-react'
+import Header from './_components/header'
+import { Button } from './_components/ui/button'
+import { Input } from './_components/ui/input'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <div>
       <Header />
       <div className="p-5">
-        <h2 className="text-xl font-bold">Olá, André!</h2>
+        <h2 className="text-xl font-bold"> Olá, André!</h2>
         <p>Sexta-feira, 09 de agosto.</p>
 
-        <div className="flex flex-row items-center gap-2 mt-6">
-          <Input placeholder="Pesquisar"/>
+        <div className="mt-6 flex flex-row items-center gap-2">
+          <Input placeholder="Pesquisar" />
           <Button>
             <SearchIcon />
           </Button>
         </div>
 
         <div className="relative mt-6 h-[150px] w-full">
-          <Image alt="Banner 01" src="/banner-01.png" fill className="rounded-xl object-cover" />
+          <Image
+            alt="Banner 01"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
         </div>
       </div>
     </div>
-  );
+  )
 }
